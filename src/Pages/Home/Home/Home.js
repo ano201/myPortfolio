b7b5aadc0c6eacaa98bc454b47ccb16df9e06650
ano201 <ahmu689@gmail.com>
2021-11-21 13:47:01 +0600

    app bar added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import { Grid, Box, Container } from '@mui/material';
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Navigation from './../Navigation/Navigation';
 
 const Home = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
+            <Navigation></Navigation>
             <Container>
                 <Grid container>
                     <Grid>
