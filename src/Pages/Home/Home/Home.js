de6185a276a35de74125c638dbcc65626619f32e
ano201 <ahmu689@gmail.com>
2021-11-21 18:22:01 +0600

    app bar responsive added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,15 +5,10 @@ import Navigation from './../Navigation/Navigation';
 
 
 const Home = () => {
-
-  return (
-    <Box sx={{ flexGrow: 1 }}>
             
-=======
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Navigation></Navigation>
->>>>>>> b7b5aadc0c6eacaa98bc454b47ccb16df9e06650
             <Container>
                 <Grid container>
                     <Grid>
@@ -21,8 +16,8 @@ const Home = () => {
                     </Grid>
                 </Grid>
             </Container>
-        </Box>
-  );
+</Box>
+  )
 };
 
 export default Home;
