321b4abd9c2fb013f459053c7cc04c8239550322
an201 <ahmu689@gmail.com>
2021-11-23 21:00:22 +0600

    hoyna kn

--- src/Pages/Home/Navigation/NavDrawer/NavDrawer.js
+++ src/Pages/Home/Navigation/NavDrawer/NavDrawer.js
@@ -1,55 +1,17 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+
 
 const NavDrawer = ({toggleDrawer, state}) => {
 
-    const list = (anchor) => (
-        <Box
-            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    )
-
     return (
-        <div>
+        <div style={{ background: '#0a192f8e'}}>
             <Drawer
                 anchor='right'
                 open={state['right']}
                 onClose={toggleDrawer('right', false)}
             >
-                {list('right')}
+                <h1>me</h1>
             </Drawer>
         </div>
     );
