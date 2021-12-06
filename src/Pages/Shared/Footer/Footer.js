1c3a7639e66d7c941582d5e7b4cbd265e403d0c9
ano201 <ahmu689@gmail.com>
2021-12-07 02:19:11 +0600

    starting footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
     return (
         <Box sx={{ flexGrow: 1, mt: '10rem' }}>
             <Typography
-                variant="h1"
+                variant="subtitle2"
                 component="div"
                 sx={{ flexGrow: 1, textAlign: { xs: 'center', sm: 'center', md: 'left' }, my: { xs: '2rem', sm: '2rem' } }}
-                className='light-text'
+                className='neon-text'
             >
-                Footer
+            Quality-focused and results-driven professional seeking a position as a web developer where I can use my skills to enhance the user experience...
             </Typography>
         </Box>
     );
