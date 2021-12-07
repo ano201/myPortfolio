4c7aa6ae966fbe63b91b97972c90c63757e81afe
ano201 <ahmu689@gmail.com>
2021-12-07 09:59:22 +0600

    f and img tinyfied

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
+import footerMe from '../../../images/footerMe.png';
 
 const Footer = () => {
     return (
         <Box sx={{ flexGrow: 1, mt: '10rem' }}>
-            <Typography
-                variant="subtitle2"
-                component="div"
-                sx={{ flexGrow: 1, textAlign: { xs: 'center', sm: 'center', md: 'left' }, my: { xs: '2rem', sm: '2rem' } }}
-                className='neon-text'
-            >
-            Quality-focused and results-driven professional seeking a position as a web developer where I can use my skills to enhance the user experience...
-            </Typography>
+            <Container>
+            <Grid>
+                <Grid sx={{ m: 'auto', weight: 1/4 }} >
+                    <img src={footerMe} />
+                </Grid>
+            </Grid>
+            <Grid>
+                <Typography
+                                variant="subtitle2"
+                                component="div"
+                                sx={{ flexGrow: 1, textAlign: { xs: 'center', sm: 'center', md: 'left' }, my: { xs: '2rem', sm: '2rem' } }}
+                                className='neon-text'
+                            >
+                            Quality-focused and results-driven professional seeking a position as a web developer where I can use my skills to enhance the user experience...
+                </Typography>
+            </Grid>
+            </Container>
         </Box>
     );
 };
