0c61366ed7cb750b6535f2302a17e3fa10998a16
ano201 <ahmu689@gmail.com>
2021-12-07 15:52:09 +0600

    footer img

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,21 +7,23 @@ const Footer = () => {
     return (
         <Box sx={{ flexGrow: 1, mt: '10rem' }}>
             <Container>
-            <Grid>
-                <Grid sx={{ m: 'auto', weight: 1/4 }} >
-                    <img src={footerMe} />
-                </Grid>
-            </Grid>
-            <Grid>
-                <Typography
-                                variant="subtitle2"
-                                component="div"
-                                sx={{ flexGrow: 1, textAlign: { xs: 'center', sm: 'center', md: 'left' }, my: { xs: '2rem', sm: '2rem' } }}
-                                className='neon-text'
-                            >
+                <Grid container>
+                    <Grid xs={12} sm={12} md={8} lg={6} xl={6} sx={{ m: 'auto' }} >
+                        <Grid sx={{ m: 'auto', width: 1 / 2 }} >
+                            <img src={footerMe} alt="" />
+                        </Grid>
+                    </Grid>
+                    <Grid xs={12} sm={12} md={6} lg={6} xl={6} sx={{ m: 'auto' }} >
+                        <Typography
+                            variant="subtitle2"
+                            component="div"
+                            sx={{ flexGrow: 1, textAlign: {xs: 'center', sm: 'center', lg: 'left', xl: 'left'}, my: { xs: '2rem', sm: '2rem' } }}
+                            className='neon-text'
+                        >
                             Quality-focused and results-driven professional seeking a position as a web developer where I can use my skills to enhance the user experience...
-                </Typography>
-            </Grid>
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Container>
         </Box>
     );
