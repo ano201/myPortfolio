675e49a6e3541fde044a82414a213b1b8787cbfd
ano201 <ahmu689@gmail.com>
2021-12-11 12:25:25 +0600

    footer completed

--- src/Pages/Shared/Asides/LeftAside.js
+++ src/Pages/Shared/Asides/LeftAside.js
@@ -4,20 +4,20 @@ import { Grid, Typography } from '@mui/material';
 
 const LeftAside = () => {
 
-	const mailAddress = 'AHMU689@GMAIL.COM';
+	const mailAddress = 'AHMU689@GMAIL•COM';
 	const stringArray = mailAddress.split("");
 
     return (
         <Box sx={{  }} >
-            <Grid sx={{ width: '2rem', height: '70vh', display: { xs: 'none', sm: 'none', md: 'none', lg: 'inline-flex', xl: 'inline-flex' }, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', position: 'fixed', border: 2 }} >
+            <Grid sx={{ width: '2rem', height: '70vh', display: { xs: 'none', sm: 'none', md: 'none', lg: 'inline-flex', xl: 'inline-flex' }, alignItems: 'center', justifyContent: 'center', flexDirection: 'column', position: 'fixed' }} >
 				<Grid>
 				<a className="link" target='blank' href='mailto:ahmu689@gmail.com'>
-               {stringArray.map(letter => <Typography className='neon-text' variant="h6" sx={{ textAlign: 'center', mt: '-1rem' }}>
+               {stringArray.map(letter => <Typography className='neon-text' variant="subtitle1" sx={{ textAlign: 'center', mt: '-12px' }}>
                     {letter}
                 </Typography>)}
             </a>
 			</Grid>
-			<Grid sx={{ height: '90%', borderLeft: 2, borderColor: '#4df1aa', mt: 1 }}>
+			<Grid sx={{ height: '90%', borderLeft: 2, borderColor: '#4df1aa', mt: '-8px' }}>
                                                             </Grid>
 			</Grid>
         </Box>
